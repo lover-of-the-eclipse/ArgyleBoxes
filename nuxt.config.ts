ead2613ffddf132de9a3d1d4fc3ac8f18f8979dd
lover-of-the-eclipse <notxenogenisis@gmail.com>
2026-02-14 22:19:12 -0500

    updated baseURL in config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,8 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
+  app: { baseURL: '/ArgyleBoxes/', buildAssetsDir: 'assets' },
+
   ssr: false,
 
   nitro: {
