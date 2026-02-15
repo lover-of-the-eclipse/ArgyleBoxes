52c2f861210e7d08a7b90a352b8797a454c68eec
KaiFu7k <kaifu7k@gmail.com>
2026-02-14 22:15:27 -0500

    Revert "attempting to fix"
    This reverts commit 64460892de0d79aaceae3f68225929425d0461a2.

--- app/pages/unlocked.vue
+++ app/pages/unlocked.vue
@@ -1,24 +1,35 @@
 <script setup lang="ts">
-// You no longer need the store or router here for the guard!
+import { usePasscodeStore } from "~/stores/passcodeStore";
 
-// This special function tells Nuxt to run the 'auth' middleware before
-// rendering this page. The filename 'auth.ts' becomes 'auth'.
-definePageMeta({
-  middleware: 'auth'
-})
+const router = useRouter()
+const passcodeStore = usePasscodeStore()
+
+const ready = ref(false)
 
 useHead({
   title: 'Geryon was a monster'
 })
+
+// checks if actually unlocked before rendering any content.
+onMounted(() => {
+  // if user did not go through passcode puzzle, then they are redirected back to the passcode
+  // likely not entirely secure, but good enough for now, honestly.
+  if (!passcodeStore.unlocked) {
+    router.push("/")
+  } else {
+    ready.value = true
+  }
+})
 </script>
 
 <template>
-  <div>
-    <div class="p-4 text-2xl">
-      Geryon was a monster everything about him was red.
-    </div>
+<div v-if="ready">
+  <div class="p-4 text-2xl">
+    Geryon was a monster everything about him was red.
   </div>
+</div>
 </template>
 
 <style scoped>
+
 </style>
