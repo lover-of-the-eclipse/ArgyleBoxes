830d6283ae510e18ef0ccff741c01a1e658ef581
lover-of-the-eclipse <notxenogenisis@gmail.com>
2026-02-14 20:34:30 -0500

    added basic keypad, unlocking mechanisms through Pinia storage, and some really basic css for buttons, organization, and animations.

---
+++ app/pages/unlocked.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { usePasscodeStore } from "~/stores/passcodeStore";
+
+const router = useRouter()
+const passcodeStore = usePasscodeStore()
+
+const ready = ref(false)
+
+useHead({
+  title: 'Geryon was a monster'
+})
+
+// checks if actually unlocked before rendering any content.
+onMounted(() => {
+  // if user did not go through passcode puzzle, then they are redirected back to the passcode
+  // likely not entirely secure, but good enough for now, honestly.
+  if (!passcodeStore.unlocked) {
+    router.push("/")
+  } else {
+    ready.value = true
+  }
+})
+</script>
+
+<template>
+<div v-if="ready">
+  <div class="p-4 text-2xl">
+    Geryon was a monster everything about him was red.
+  </div></div>
+</template>
+
+<style scoped>
+
+</style>
